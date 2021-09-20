9fad5c971c296fa0e2f066be9ddb08779ab81d95
Ahmednoor114 <60637755+Ahmednoor114@users.noreply.github.com>
2021-09-20 16:49:08 -0500

    Initial commit

---
+++ src/main/java/com/abdisg/helloworld/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Ahmednoor Abdi
+ * Email : galangale1@gmail.com
+ * Date  : 09/20/2021
+ * purpose: My first Hello World program in an IDE
+ * 
+ */
+package com.abdisg.helloworld;
+
+/**
+ *
+ * @author ahmednoorabdi
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("Hellow, Ahmednoor!");
+    }
+    
+}
